1ed55990c5895b2ebda57fe10ae79eff523af8f9
Udeni dilrukshi <40499515+udenidilru@users.noreply.github.com>
2019-10-21 00:41:22 +0530

    added queries

--- application/models/query.php
+++ application/models/query.php
@@ -1,7 +1,18 @@
 <?php
 	class query extends CI_Model{
 
-		
+		public function getRoles(){
+			$roles = $this->db->get('roles');
+			if($roles->num_rows() > 0){
+				return $roles->result();
+			}
+		}
+		public function getColleges(){
+			$colleges = $this->db->get('college');
+			if($colleges->num_rows() > 0){
+				return $colleges->result();
+			}
+		}
 
 		public function chkAdminExist(){
 			$chkAdmin = $this->db->where(['role_id' => '1'])->get('users');
@@ -14,6 +25,51 @@
 			if($chkAdmin->num_rows()>0){
 				return $chkAdmin->row();
 			}
+		}
+		public function makeCollege($data){
+			return $this->db->insert('college',$data);
+		}
+
+		public function registerAdmin($data){
+			return $this->db->insert('users',$data);
+		}
+		public function registerCoadmin($data){
+			return $this->db->insert('users',$data);
+		}
+
+		public function viewAllColleges(){
+			$this->db->select(['users.user_id','users.email','college.college_id','users.user_name','users.gender','college.collegename','college.branch','roles.role_name']);
+			$this->db->from('college');
+			$this->db->join('users','users.college_id = college.college_id');
+			$this->db->join('roles','roles.role_id = users.role_id');
+			$users = $this->db->get();
+			return $users->result();
+			
+		}
+		public function insertStudent($data){
+			return $this->db->insert('students',$data);
+		}
+
+		public function getStudents($college_id){
+			$this->db->select([ 'students.id','students.studentname','college.collegename','students.email','students.gender','students.subject']);
+			$this->db->from('students');
+			$this->db->join('college','college.college_id = students.college_id');
+			$this->db->where(['students.college_id' => $college_id]);
+			$students = $this->db->get();
+			return $students->result();
+		}
+
+		public function getStudentRecord($id){
+			$this->db->select(['college.college_id','college.collegename','students.id','students.email','students.gender','students.studentname','students.subject']);
+			$this->db->from('students');
+			$this->db->join('college','college.college_id = students.college_id');
+			$this->db->where(['students.id' => $id]);
+			$student = $this->db->get();
+			return $student->row();
+		}
+
+		public function removeStudent($id){
+			return $this->db->delete('students',['id' => $id]);
 		}
 		
 	}
